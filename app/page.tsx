1de5b8967594578b55cb044fb03f35ee8254a87e
Rumman <rummanfardeen4567@gmail.com>
2024-12-23 10:43:20 +0530

    Scroll and Grid Section added ✨

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import Hero2 from "@/components/Hero2";
 import Explosion from "@/components/Explosion";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import ScrollSection from "@/components/ScrollSection";
+import CardGridSection from "@/components/CardGridSection";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero2 />
-      <Explosion />
+      <ScrollSection/>
+      <CardGridSection/>
     </>
   );
 }
